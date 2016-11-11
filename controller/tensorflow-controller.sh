5a5224d4ebd44c70199f0c3f76af37ae355ea1d5
Elson Rodriguez <elson.rodriguez@gmail.com>
2016-11-11 13:58:25 -0800

    Adding some structure, and seperating out v0 vs v1 prototpying.

---
+++ controller/tensorflow-controller.sh
@@ -0,0 +1,7 @@
+# check for tensorsets in all namespaces that do not have corresponding rcs/svcs
+
+# create object template via scripts/k8s_tensorflow.py
+
+# submit object yaml to api under the tensorset's namespace
+
+# clean up wayward objects by checking all namespaces for objects with tensorset labels that do not have corresponding tensorset objects
